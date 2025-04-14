b6e57bf9e1910c15031e41de2380dbd729f9b6ab
Piotr Drabik <pdrabik223@gmail.com>
2025-04-14 20:46:55 +0200

    routing between pages

--- src/main.py
+++ src/main.py
@@ -3,19 +3,18 @@ import json
 import socket
 import time
 from wifi_tools import connect_to_wifi
-from led_strip import LedStrip
+from led_strip import LedStrip, Color
 
 led_strip = LedStrip()
+led_strip.fill(Color(242,164,28))
 
-def load_html(path_to_html_file:str = 'assets/index.html'):
+def load_html(path_to_html_file:str = 'assets/index.html')->str:
     html = """<!DOCTYPE html><html><head><title>failed to load html data</title></head></html>"""
     
     with open(path_to_html_file, 'r') as file:
-        html_str =  file.read()
+        html_str = file.read()
 
     return  html_str
-
-html = load_html()
 
 ip = connect_to_wifi()
 addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
@@ -23,40 +22,65 @@ s = socket.socket()
 s.bind(addr)
 s.listen(1)
 
-
-# @endpoint("")
-def home_page(cl):
+def home_page(cl, params, named_params):
     print("home page")
-    cl.send(html)
+    page_str = load_html("assets/index.html")
+    cl.send(page_str.format(example_variable =  "Variable secured"))
     
-    
+def test2_page(cl, params, named_params):
+    page_str = load_html("assets/index.html")
+    print("returning test2 page")
+    cl.send(page_str.format(example_variable =  "TEST2"))
 
-routes_map = {"": home_page}
+def page_not_found(cl):
+    cl.send(load_html("assets/404_page.html"))
 
-def endpoint(path:str):
-    def endpoint_decorator(func):
-        def wrapper(*args, **kwargs):
-            print("wrapper")
-            routes_map[path] = lambda : func(*args, **kwargs)
-            return
-        return wrapper
-    return endpoint_decorator
+routes_map = {"/": home_page, "/test2":test2_page}
+
+def parse_uri(uri:str):
+    params_separator = uri.find("?")
+    if params_separator == -1:
+        return uri, [], {}
     
+    path = uri[:params_separator]
+    params = uri[params_separator+1:].split('&')
+    
+    positional_params = []
+    named_parameters = {}
+    
+    for param in params:
+        if param.find('=') == -1:
+            positional_params.append(param)
+        else:
+            named_parameters[param.split("=")[0]] = param.split("=")[1]
+            
+    return path.strip(), positional_params, named_parameters
+
 def redirect(ip , cl):
     
     request = cl.recv(1024)
-    begin = str(request).find("Referer:")
+    begin = str(request).find("GET")
     referer_str = str(request)[begin:].split("\\n")[0]
-    route_str = referer_str[8:-2]
+    referer_str = referer_str[3:-10]
 
-    route_str = route_str[len("http://") + len(ip) + 1:].strip()
-    print(route_str)
+    route_str = referer_str.strip()
+    path, positional_params, named_parameters = parse_uri(route_str)
+    print(f"full request: {str(request)}")
+    print("path:" , path) 
+    print("params:" , positional_params)
+    print("named_params:" , named_parameters)
+    
+    if path not in routes_map:
+        print(f"page: {path} was not in routes_map")
+        # page_not_found(cl)
+        return
+    
     try:
-        routes_map[route_str](cl)
+        routes_map[path](cl, positional_params, named_parameters)
     except Exception as err:
         print(err)
     
-print(f"listening on: http://{ip}\nlist of endpoints: {routes_map}")
+print(f"listening on: http://{ip}")
 
 while True:
     try:
